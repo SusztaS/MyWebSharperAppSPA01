49662ee5e09d1c4a5bfc2c8914f3a5de772a9b99
Sándor Suszta <sandor.suszta@gmail.com>
2026-03-14 22:21:53 +0100

    v0.1

---
+++ Client.fs
@@ -0,0 +1,60 @@
+﻿namespace MyWebSharperAppSPA01
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.UI
+open WebSharper.UI.Client
+open WebSharper.UI.Templating
+
+[<JavaScript>]
+module Client =
+
+    type ViewState = AdatView | ListaView | SorsolasView
+
+    type IndexTemplate = Template<"wwwroot/index.html", ClientLoad.FromDocument, ServerLoad.WhenChanged>
+
+    let People = ListModel.FromSeq [ "John"; "Paul" ]
+
+    [<SPAEntryPoint>]
+    let Main () =
+        let currentView = Var.Create AdatView
+        let newName = Var.Create ""
+
+        IndexTemplate.Main()
+            .ShowAdat(fun _ -> currentView.Value <- AdatView)
+            .ShowLista(fun _ -> currentView.Value <- ListaView)
+            .ShowSorsolas(fun _ -> currentView.Value <- SorsolasView)
+
+            .MainContent(
+                currentView.View.Map(fun view ->
+                    match view with
+                    | AdatView ->
+                        IndexTemplate.AdatView()
+                            .ListContainer(
+                                People.View.DocSeqCached(fun (name: string) ->
+                                    IndexTemplate.ListItem().Name(name).Doc()
+                                )
+                            )
+                            .Name(newName)
+                            .Add(fun _ ->
+                                if newName.Value.Trim() <> "" then
+                                    People.Add(newName.Value)
+                                    newName.Value <- ""
+                            )
+                            .Doc()
+                    
+                    | ListaView ->
+                        Doc.Element "div" [] [ 
+                            Doc.Element "h1" [Attr.Class "h3"] [ Doc.TextNode "Versenyzők listázása" ]
+                            Doc.TextNode "Itt jelenik majd meg a végleges lista táblázata." 
+                        ]
+                    
+                    | SorsolasView ->
+                        Doc.Element "div" [] [ 
+                            Doc.Element "h1" [Attr.Class "h3"] [ Doc.TextNode "Sorsolás" ]
+                            Doc.TextNode "A sorsolási funkció hamarosan elérhető lesz." 
+                        ]
+                ) |> Doc.EmbedView
+            )
+            .Doc()
+        |> Doc.RunById "main"
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+﻿module.exports = {
+  root: "wwwroot",
+  build: {
+    rollupOptions: {
+      input: [
+        "./Scripts/MyWebSharperAppSPA01.min.js"
+      ]
+    }
+  }
+}
